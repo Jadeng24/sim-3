f2c3b9132776b29fe8973b988d32385f49a70768
Jaden Goodwin <jaden.goodwin24@gmail.com>
2017-10-06 13:52:26 -0600

    router stuff

--- src/router.js
+++ src/router.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import Login from './components/login/Login.js'
+import Login from './components/login/Login.js';
+import Dashboard from './components/dashboard/Dashboard.js';
+import Profile from './components/profile/Profile.js';
+import Search from './components/search/Search.js';
 
 export default (
     <div>
     
     <Switch>
-        <Route component={ Login } exact path='/'/>
+            <Route component={Login} exact path='/' />
+            <Route component={Dashboard} path='/dashboard' />
+            <Route component={Profile} path='/profile' />
+            <Route compoent={Search} path='/search' />
             
     </Switch>
     </div>
