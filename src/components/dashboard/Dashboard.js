905c324a53e149f21a9c3bfe954c540da3b93f40
Jaden Goodwin <jaden.goodwin24@gmail.com>
2017-10-06 15:58:50 -0600

    pretty stuff

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -14,12 +14,29 @@ export default class Dashboard extends Component {
                         <div className="profileLink boxes">
                             <div className="profileImg"></div>
                             <div className="profileLinkContent"><p className="profileP">first</p><p className="profileP">last</p>
-                                <Link to="/"><div className="editProfileBtn">Edit Profile</div></Link>
+                                <Link to="/profile"><div className="editProfileBtn">Edit Profile</div></Link>
                             </div>    
                         </div>
                         <div className="welcome boxes">Welcome to Helo! Find recommended friends based on your similarities, and even search for them by name. The more you update your profile, the better recommendations we can make!</div>
                     </div>
-
+                    <div className="dashBottom">
+                        <div className="friendsDiv boxes">
+                            <div className="friendsTop">
+                                <p>Friends</p>
+                                <p> SortedBy </p>
+                                <select>
+                                    <option> First Name </option>
+                                    <option> Last Name </option>    
+                                    <option> Relationship </option>    
+                                    <option> Hobby </option>    
+                                    <option> Hair Color </option>    
+                                    <option> Eye Color </option>    
+                                    <option> Birthday </option>    
+                                </select>    
+                            </div>
+                            
+                        </div>
+                    </div>    
                 </div>
             </div>
         )
