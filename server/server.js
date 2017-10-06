5f4aabb20a58c03dc7427368d28c9dc5060a8b66
Mikel North <mikel.north@gmail.com>
2017-10-06 12:41:21 -0600

    server, auth0

--- server/server.js
+++ server/server.js
@@ -56,7 +56,7 @@ const express = require('express'),
     app.get('/auth', passport.authenticate('auth0'));
     
     app.get('/auth/callback', passport.authenticate('auth0', {
-        successRedirect: 'http://localhost:3000/',
+        successRedirect: 'http://localhost:3000/#/dashboard',
         failureRedirect: '/auth'
     }));
     app.get('/auth/me', (req, res) => {
